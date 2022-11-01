a4da55892bc47a46bc4b1f699b1d91fd1198e7e0
Himank Jain <himankjain@yahoo.in>
2022-11-01 13:12:33 +0530

    added clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use clap::{Parser, ValueEnum};
+
+#[derive(Copy, Clone,Debug, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+enum Converters {
+    Hex
+}
+
+/// Drsta CLI for converstion
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+   ///Value to convert
+   #[arg(short, long)]
+   value: u128,
+
+   ///Type to convert
+   #[arg(short, long,value_enum)]
+   to: Converters
+}
+
 fn main() {
-    println!("Hello, world!");
+   let args = Cli::parse();
+   
+   println!("{}", args.to);
+
 }
